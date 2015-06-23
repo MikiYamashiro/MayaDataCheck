594ef6b7f463e484820a6d383673f9e0363e749b
山城 美希 <yamashiromiki@yamajiro-no-iMac.local>
2015-06-23 22:58:56 +0900

    added function directory, added checkUVSet.py

---
+++ functions/checkUVSet.py
@@ -0,0 +1,31 @@
+from maya import cmds
+
+
+def getList(typestr):
+    return cmds.ls(type=typestr)
+    
+
+def searchUVSet(multiple_UV_set, not_UV_set):    
+    # get mesh list    
+    for mesh in getList('mesh'):
+        uvset_list = cmds.polyUVSet(mesh, query=True, allUVSets=True)
+        uvset_count = len(uvset_list)
+        if uvset_count == 1:
+            continue
+        # Multiple UVSet
+        elif uvset_count > 1:
+            uvname_list = []
+            for uvset_name in uvset_list:
+                uvname_list.append(uvset_name)
+            multiple_UV_set.update({mesh: uvname_list})
+        # Not has UVSet
+        elif uvset_count == 0:
+            not_UV_set.append(mesh)
+
+# Main Function
+def run():
+    multiple_UV_set = {}
+    not_UV_set = []    
+    searchUVSet(multiple_UV_set, not_UV_set)
+    
+    return multiple_UV_set, not_UV_set
